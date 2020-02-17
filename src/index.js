a8d74dc2b1f7c2af38686b5b5ac6afec10bfc156
volb <700679+volb@users.noreply.github.com>
2020-02-17 11:23:20 -0800

    Initial commit on rudakeest project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+class Menu extends React.Component {
+
+}
+
+class MenuOption extends React.Component {
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
