9d907f0aadced9655a0012ff93ffe6b62528f4cf
volb <700679+volb@users.noreply.github.com>
2020-03-18 20:21:39 -0700

    Fix toggle for animate button

--- src/index.js
+++ src/index.js
@@ -5,52 +5,70 @@ import './menu.css';
 import * as serviceWorker from './serviceWorker';
 
 class InputForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        poem: '',
-        splitPoem: [],
-        name: props.name,
-        poemSubmitted: false
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({poem: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      event.preventDefault();
-      this.setState({poem: event.target.value});
-      this.setState({splitPoem: this.state.poem.split('\n')});
-      this.setState({poemSubmitted: true});
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      poem: '',
+      splitPoem: [],
+      name: props.name,
+      poemSubmitted: false
+    };
 
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Enter the {this.state.name}: {JSON.stringify(this.state.splitPoem)}
-            {this.state.poem}
-            <textarea value={this.state.poem} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ poem: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ poem: event.target.value });
+    this.setState({ splitPoem: this.state.poem.split('\n') });
+    this.setState({ poemSubmitted: true });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Enter the {this.state.name}: {JSON.stringify(this.state.splitPoem)}
+          {this.state.poem}
+          <textarea value={this.state.poem} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 class MainBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAnimated: false,
+      animationText: "Animate"
+    };
+    this.toggleAnimation = this.toggleAnimation.bind(this);
+  }
+
+  toggleAnimation() {
+    this.setState({ isAnimated: !this.state.isAnimated });
+    this.setState({ animationText: this.state.isAnimated ? "Animate" : "Minimize" });
+  }
+
   render() {
     return (
-    <div class="main-box">
-    <Menu />
-    <InputForm name="Poem" />
-    <InputForm name="Translation" />
-    </div>
+      <div class="main-box">
+        <Menu />
+        <InputForm name="Poem" />
+        <InputForm name="Translation" />
+    <button onClick={this.toggleAnimation}>
+          {this.state.animationText}
+    </button>
+    <p>{JSON.stringify(this.state.isAnimated)}</p>
+      </div>
     );
   }
 }
