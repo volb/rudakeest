9834d8dee50ef4d8061f2d9e5e220b1272abb40d
volb <700679+volb@users.noreply.github.com>
2020-05-04 19:36:19 -0700

    Write two solutions to api server

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const port = 3001;
+const balkhiate = require('balkhiate');
 app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/build/index.html'));
 });
-
-app.listen(3001);
+const test = balkhiate.scrapePoem("rumi");
+const testB = test.then((result) => console.log(result));
+// below does not work
+let something = async() => {
+    let result = await balkhiate.scrapePoem();
+    return result;
+ }
+ 
+app.listen(port,testB);
