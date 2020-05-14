38f83607e90cbd17d64274019cad06cd7797136c
volb <700679+volb@users.noreply.github.com>
2020-05-14 15:46:03 -0700

    Add package json and fix path

--- server/server.js
+++ server/server.js
@@ -7,10 +7,10 @@ const balkhiate = require('balkhiate');
 
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/build/index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 const test = balkhiate.scrapePoem("rumi");
