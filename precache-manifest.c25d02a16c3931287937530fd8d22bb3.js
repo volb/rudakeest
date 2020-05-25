d46de3159389d599d35201e0c873ef05bbda7117
volb <700679+volb@users.noreply.github.com>
2020-05-24 22:39:54 -0700

    Fix paths

---
+++ precache-manifest.c25d02a16c3931287937530fd8d22bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5819dd4d3c5d1fc399e0fbc2e3d60045",
+    "url": "/rudakeest/index.html"
+  },
+  {
+    "revision": "08dae2a75f652479f34e",
+    "url": "/rudakeest/static/css/main.ab5af68e.chunk.css"
+  },
+  {
+    "revision": "03fe762ebe18f425e021",
+    "url": "/rudakeest/static/js/2.907335a5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/rudakeest/static/js/2.907335a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08dae2a75f652479f34e",
+    "url": "/rudakeest/static/js/main.fa689807.chunk.js"
+  },
+  {
+    "revision": "d68ec248bbdf308b42a5",
+    "url": "/rudakeest/static/js/runtime-main.5a9a39b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d2d6c025860781c3e110141dfbc9c15.js"
+  "/rudakeest/precache-manifest.c25d02a16c3931287937530fd8d22bb3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rudakeest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
